9d65dc06e0afa885d88f86d4f9414ee3964a70c9
picolake <119658930+picolake@users.noreply.github.com>
2024-04-30 20:05:40 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php require("php/layout_head.php"); ?>
+
+<!-- 版身開始 -->
+<div class="bodyArea">
+    <div class="body-L">
+        <!-- 選單開始 -->
+        <?php require("php/layout_sel.php"); ?>
+        <!-- 選單結束 -->
+    </div><!-- body-L End -->
+    <div class="body-R">
+        <!-- 版身內容開始 -->
+        <!-- 版身內容結束 -->
+    </div><!-- body-R End -->
+</div><!-- bodyArea End -->
+<!-- 版身結束 -->
+
+<?php require("php/layout_footer.php"); ?>
